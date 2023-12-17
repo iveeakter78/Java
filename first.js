71f94e6da8b7cead397fd21d3a37de54f4824bf2
User <f82058878@gmail.com>
2023-12-17 11:10:51 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,4 @@
+var roll = 1234;
+var regi = 100000876;
+var name = "abcd";
+console.log(roll);
---
+++ practise.js
@@ -0,0 +1,548 @@
+//  var tanjidHasan = 84;
+//  var littonDas = 61;
+//  var miraz = 67;
+//  var mushfiq = 35;
+//  var hridoy = 0;
+
+//  var teamName = "Bangladesh";
+
+//  var isHappy = false;
+
+//  var type = typeof mushfiq;
+//  var type = typeof false;
+
+
+//  var totalScore = tanjidHasan + littonDas + miraz + mushfiq + hridoy;
+//  var average = totalScore /5;
+
+//  console.log('Total Score:' + '' + totalScore);
+//  console.log(average);
+// console.log(teamName);
+// console.log(mushfiq);
+// console.log(false);
+
+//  var num1 = 0.1;
+
+//  var num2 = 0.2;
+
+//  var total = num1 + num2;
+
+//  var total = total.toFixed(1);
+//  console.log(total);
+// Array //
+//  var amount = [100, 90, 80, 70, 60, 66];
+//  var total = amount.length;
+//  console.log(total);
+
+//  var num1 = 0.1;
+
+//  var num2 = 0.2;
+
+//  var total = num1 + num2;
+
+//  total = total.toFixed(1);
+
+//  total = parseFloat(total);
+
+//  console.log(total);
+
+// var amount = [100, 90, 80, 70, 60, 66];
+// var total = amount.length;
+
+// console.log(total);
+
+
+
+
+
+
+//  var age = [23, 45, 56, 67, 78, 89, 90];
+
+//  var positionIndex = age[0];
+
+//   var positionIndex = age.indexOf(78);
+
+
+//   console.log(positionIndex);
+
+//  console.log(positionIndex);
+
+// var age = [23, 45, 56, 67, 78, 89, 90, 78];
+//  console.log(age);
+
+//  age.push(89);
+
+//  console.log(age);
+
+//  var age = [23, 45, 56, 67, 78, 89, 90, 78];
+
+//  console.log(age);
+
+//  age.pop();
+
+//  console.log(age);
+
+/**Operation Comparision */
+
+// + - * / %
+
+/**Condition */
+
+//  var iPhonePrice = 150000;
+//   var budget = 200000;
+
+//   if (budget > iPhonePrice) {
+//     console.log("iPhone Kinbo");
+//   } else {
+//     console.log("iPhone Kinbo na")
+//   }
+
+//   if (budget > iPhonePrice) {
+//     console.log("iPhone Kinbo");
+//   } else {
+//     console.log("iPhone Kinbo na")
+//   }
+
+// var iPhonePrice = 150000;
+
+// var android = 20000;
+
+// var budget = 30000;
+
+// if (budget>iPhonePrice) {
+//     console.log(i phone kinbio);
+// }
+
+// var age = [23, 45, 67, 77, 78, 90, 87];
+
+// console.log(age);
+
+// age.unshift(56);
+
+// console.log(age);
+
+// var age = [67, 45, 67, 77, 78, 90, 87];
+
+// console.log(age);
+
+// age.shift(67);
+
+// console.log(age);
+
+// var age = [23, 34, 45, 56, 78, 99, 90];
+
+// console.log(age);
+
+// age.splice(2, 0, 67);
+
+// console.log(age);
+
+//   var age = [23, 34, 45, 56, 78, 99, 90];
+
+//   console.log(age);
+
+//   age.splice(3, 56);
+
+//   console.log(age);
+
+
+//loop
+
+// var num = 0;
+
+// while (num < 5){
+//     console.log("to be continued");
+//     num = num + 1;
+//     // num += 1;
+//     // num++;
+// }
+
+// var num = 2;
+
+// while (num <= 10){
+//     console.log(num);
+//     num += 2;
+
+// }
+
+// var num = 3;
+
+// while (num <= 13){
+//     console.log(num);
+//     num += 2;
+// }
+
+// var num = 0;
+
+// while (num <= 90){
+//     console.log(num);
+//     num += 1;
+// }
+
+// for (var num = 1; num <= 10; num++){
+//     console.log(num);
+// }
+
+// for (var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// var numbers = [12, 23, 34, 45, 56, 67, 78];
+
+// for (i = 0; i <= 6; i++){
+//     number = numbers[i];
+//     console.log(number);
+// }
+
+// // problem 1 //
+
+// var num = 1000;
+// var cost = 700;
+// var total = (num - cost);
+
+// console.log(total);
+
+// problem 2 //
+
+//  var total = [75.25, 65, 80, 35.45, 99.50];
+
+//  var equation = (75.25 + 65 + 80 + 35.45 + 99.50);
+
+//  console.log(equation);
+
+//       average = (equation / 5);
+//      average = average.toFixed(2);
+
+//       console.log(average);
+
+// // problem 3 //
+
+//  var num = "I am going to be";
+
+//  var numb = " AN awesome web developeer";
+
+// var para = (num + numb);
+// console.log(para);
+
+// problem 4 //
+
+//  var fruits = ["Apple", "Banana", "Orange"];
+// bananaIndex = fruits.indexOf("Banana");
+// fruits[bananaIndex] = "Mango";
+// console.log(bananaIndex);
+// console.log(fruits);
+
+
+
+// var fruits = ["Apple", "Banana", "Orange"];
+// console.log(fruits);
+
+// fruits.pop( "Orange");
+// console.log(fruits);
+
+// fruits.push("Watermelon");
+// console.log(fruits);
+
+// problem 5 //
+//  var me = 80;
+//  var tom = 66;
+//  var jane = 95;
+//  var peter = 56;
+//  var john = 40;
+
+//  var number = [80, 66, 95, 56, 40];
+//  console.log(number);
+
+//  var number = [80, 66, 95, 56, 40];
+//  var grade = 95;
+
+//  if (number <= grade){
+//     console.log('A+')
+//  }
+
+// var number = [85, 66, 95, 56, 40];
+// console.log(number);
+
+// var number = [85, 66, 95, 56, 40];
+// console.log(number);
+//   var number = [85, 66, 95, 56, 40];
+//   console.log(number);
+//   var grade = (number>95);
+//   console.log(grade);
+// problem-5
+//   var me = 85;
+//   var Tom = 66;
+//   var Jane = 95;
+//   var Peter = 56;
+//   var John = 40;
+
+// var number = [85, 66, 95, 56, 40];
+// var grade = 95;
+// if (number => 95){
+//    console.log('A+')
+// };
+// if (number => 85){
+//    console.log('A')
+// };
+// if (number => 60){
+//    console.log('B')
+// };
+// if (number => 50){
+//    console.log('D')
+// };
+// if (number => 40){
+//    console.log('E')
+// };
+// if (number => 39){
+//    console.log('F')
+// };
+// var iPhone15Price = 100000;
+// var budget = 1000;
+
+// if (iPhone15Price == budget) {
+//    console.log("iPhone Kinbo");
+// } else {
+//    console.log("iPhone Kinbona");
+// // }
+// var num1 = 100;
+// var num2 = 95;
+// var num3 = 43;
+// if (num1 > num2) {
+//    if (num1 > num3) {
+//       console.log("The largest number is:", num1);
+//    } else {
+//       console.log("The largest numberm is:", num3);
+//    }
+// } else {
+//    if (num2 > num3) {
+//       console.log("The largest number is:", num2);
+//    } else {
+//       console.log("The largest number is:", num3);
+//    }
+// }
+// var num2 = 95;
+// var num3 = 10;
+// if (num2 > num3) {
+//    console.log("The largest number is", num2);
+// } else {
+//    console.log("The largest number is", num3);
+// }
+
+// var num1 = 56;
+// var num2 = 300;
+// var num3 = 350;
+// if (num1 > num2) {
+//    console.log("The largest number is:", num1);
+// } else {
+//    console.log("The largest number is:", num2);
+// }
+
+// if (num2 > num3) {
+//    console.log("The largest number is:", num2);
+// } else {
+//    console.log("The largest number is:", num3);
+// }
+
+// var num1 = 100;
+// var num2 = 95;
+// var num3 = 43;
+// // if (num1 > num2 || num1 > num3) {
+// //    console.log("The largest number is:", num1);
+// // } else {
+// //    if (num2 > num3) {
+// //       console.log("The largest number is:", num2);
+// //    } else {
+// //       console.log("The largest number is:", num3);
+// //    }
+// // }
+
+// function num1(a, b, c) {
+//    var total = a + b + c;
+//    console.log(total);
+// }
+// num1(100, 98, 10);
+//  rating
+
+// var iPhone = {
+//    Model: 15,
+//    Storage: 256,
+//    Price: 100
+// }
+
+// var samsung = {
+//    Model: 21,
+//    Storage: 128,
+//    Price: 50
+// }
+
+
+// console.log(iPhone.Model, samsung.Model);
+
+// iPhone.Model = 16;
+// samsung.Model = 22;
+
+// console.log(iPhone.Model, samsung.Model);
+// var iPhone = {
+//    Model: 15,
+//    Storage: 256,
+//    Price: 100
+// }
+// var samsung = {
+//    Model: 21,
+//    Storage: 128,
+//    Price: 22,
+// }
+// console.log(iPhone.Model, samsung.Model);
+
+// // iPhone.Model = 16;
+// // samsung.Model = 22;
+
+// // console.log(iPhone.Model, samsung.Model);
+
+// iPhone.Model = 16;
+// samsung.Model = 22;
+// console.log(iPhone.Model, samsung.Model);
+
+// var inch = 12;
+// var feet = inch / 12;
+// console.log(feet);
+
+// function inchToFeet(inch) {
+//     var feet = inch / 12;
+//     console.log(feet);
+// }
+
+// inchToFeet(33);
+
+// var num = 10;
+// var num = 5;
+
+// console.log(num);
+
+// let num = 6;
+// let num1 = 9;
+// console.log(num);
+// console.log(num1);
+
+// var inch = 34;
+// var feet = inch / 12;
+// console.log(feet);
+
+// function inchToFeet(inch) {
+//     const feet = inch / 12;
+//     return feet;
+// }
+// // var user = 36;
+// // let result = inchToFeet(user);
+// // console.log(result);
+
+// var enterInch = 12;
+// var myFeet = inchToFeet(enterInch);
+// console.log(myFeet);
+
+// var centigrade = 24;
+// var farenhite = (centigrade) * 9 / 5 + 32;
+// // console.log(farenhite);
+
+// function centiToFaren(centigrade) {
+//     faren = (centigrade) * 9 / 5 + 32;
+//     return faren;
+// }
+
+// var user = 2;
+// let result = centiToFaren(user);
+// console.log(result);
+
+// var entercenti = 22;
+// var myFaren = centiToFaren(entercenti);
+// console.log(myFaren);
+
+// farenhite to centigrade
+
+// var faren = 23;
+// var centi = ((faren) - 32) * 5 / 9;
+// console.log(centi);
+
+// function farenToCenti(faren) {
+//     centi = ((faren) - 32) * 5 / 9;
+//     return centi;
+
+// }
+
+// // var user = 67;
+// // var result = farenToCenti(user);
+// // console.log(result);
+
+// var enterfaren = 36;
+// var myCenti = farenToCenti(enterfaren);
+// console.log(myCenti);
+
+// // // leap year
+// function isLeapYear(year) {
+//     if (year % 4 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// var inputYear = 2025;
+// var leapYear = isLeapYear(inputYear);
+// console.log(leapYear);
+
+// even or odd
+
+// var num = 12;
+// var even = 12 / 2;
+// console.log(even);
+
+// var miter = 23;
+// var kilo = miter * 1000;
+// console.log(kilo);
+
+// function miterToKilo(miter) {
+//     kilo = miter * 1000;
+//     return (kilo);
+// }
+
+// var miter = 20;
+// var result = miterToKilo(miter);
+// console.log(result);
+
+
+
+
+// function inchToFeet(inch) {
+//     const feet = inch / 12;
+//     return feet;
+// }
+// var user = 36;
+// let result = inchToFeet(user);
+// console.log(result);
+
+
+// kilo to mit
+
+// function kiloToMit(kilo) {
+//     const mit = kilo / 1000;
+//     return mit;
+// }
+
+// var user = 67000;
+// let result = kiloToMit(user);
+// console.log(result);
+
+// odd or even
+
+// function oddOrEven(num) {
+//     if (num % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// var enterInput = 23;
+// let result = oddOrEven(enterInput);
+// console.log(result);
+
